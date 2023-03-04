c36f26a3e643eab03323ddde905f3d9e0a044e61
pradeep-pers <prpradeep.panda@gmail.com>
2023-03-04 15:51:05 +0530

    GitBranching

---
+++ src/Calculation/TestCalc.java
@@ -0,0 +1,14 @@
+package Calculation;
+
+public class TestCalc {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int sum= a+b;
+		System.out.println(" Print the Sum:"+sum);
+
+	}
+
+}
